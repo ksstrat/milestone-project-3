56dd3753286e182519cbfa643c1f5c4b07ee51cb
ksstrat <qpvgrwku@students.codeinstitute.net>
2025-05-17 10:22:13 +0200

    refactor: remove debug prints in ship.py

--- ship.py
+++ ship.py
@@ -38,20 +38,3 @@ class Ship:
         Sets the ship's coordinates.
         """
         self.coordinates = new_coordinates
-
-
-# Testing
-if __name__ == '__main__':
-        print("Ship module test:")
-        print("Available ship configurations:", Ship.ALL_SHIPS)
-
-        # Test Coordinates
-        test_ship = Ship("Test Ship", 3)
-        print(f"Ship created: {test_ship.name}, Size: {test_ship.size}, Coordinates: {test_ship.coordinates}")
-
-        test_coords = [(0, 0), (0, 1), (0, 2)]
-        test_ship.set_coordinates(test_coords)
-        print(f"After set coords: {test_ship.name}, Coords: {test_ship.coordinates}")
-
-        test_ship.take_hit()
-        print(f"{test_ship.name} - Hits: {test_ship.hits}, Sunk: {test_ship.is_sunk}")
